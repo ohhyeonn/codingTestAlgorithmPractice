99be2504407a7e5f5c3193cfedbeb7134a3e2c55
ohhyeon <hyunong2@gmail.com>
2022-07-25 23:28:31 +0900

    서현오 답지 추가

--- 알고리즘 스터디폴더/03_01_01_큰수의법칙_답지_서현오.py
+++ 알고리즘 스터디폴더/03_01_01_큰수의법칙_답지_서현오.py
@@ -28,3 +28,63 @@
 #
 # 출력예시
 # 46
+
+
+
+# 서현오 답안
+n , m , k = map(int, input().split())
+
+data = list(map(int, input().split()))
+
+data.sort()
+
+first = data[n-1]
+second = data[n-2]
+
+result = 0
+kFlag = 0
+
+for i in range(m):
+    if kFlag >= k:
+        result += second
+        kFlag = 0
+    else :
+        result += first
+        kFlag += 1
+    
+print(result)
+
+
+#답지
+# 직관적으로 생각 가능한 방법
+n,m,k = map(int , input().split())
+data = list(map(int , input().split()))
+
+data.sort()
+first = data[len(data)-1]
+second = data[len(data)-2]
+
+result = 0
+
+while True:
+    for i in range(k) :
+        if m != 0:
+            result += first
+            m += -1
+        else:
+            break
+    if m != 0:
+        result += second
+        m += -1
+    else:
+        break
+
+print(result)
+
+
+
+
+# 비교 정리
+# 답지와의 차이점 : 답지는 연속해서 더하고 한번더하고를 while 문으로 구현 나는 그냥 m번 for 문구현 편한형식 취하면 될듯하다
+# 개선할 사항
+# 없는듯 수열로 하면 좋은데 그건 일단 논외로 두자
