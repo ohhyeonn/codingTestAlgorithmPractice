22cf9257576049f800165ec709b3f4b673d1c25c
양재혁 <yjh5128@naver.com>
2024-07-24 16:44:30 +0900

    20240724 양재혁 정렬풀이

--- src/w13/p10파일명정렬/양재혁/Solution.java
+++ src/w13/p10파일명정렬/양재혁/Solution.java
@@ -1,20 +1,32 @@
 package w13.p10파일명정렬.양재혁;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
-    public String[] solution(String[] files) {
-        String[] answer = {};
-        String[][] details = new String[files.length][3];
+    public static void main(String[] args) {
+//        String[] files = {"F- 5 Freedom Fighter", "B- 50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"};
+        String[] files = {"F-15"};
+        solution(files);
+    }
+    public static String[] solution(String[] files) {
+        String[] answer = new String[files.length];
+        Map<Integer, String[]> details = new HashMap<>();
+        Integer[] entry = new Integer[files.length];
         //나누기
         for(int i = 0; i < files.length; i++) {
             //헤드찾기
             int head = 0;
-            int tail = 0;
+            int tail = files[i].length() - 1;
             boolean found = false;
             for(int j = 0; j < files[i].length(); j++) {
                 char c = files[i].charAt(j);
-                if(c >= '0' && c <= '9' && !found) {
-                    head = j;
-                    found = true;
+                if(c >= '0' && c <= '9') {
+                    if (!found) {
+                        head = j;
+                        found = true;
+                    }
                 } else {
                     if(found) {
                         tail = j;
@@ -22,9 +34,44 @@ public class Solution {
                     }
                 }
             }
-            details[i][0] = files[i].substring(0, head - 1);
-            details[i][1] = files[i].substring(head, tail - 1);
-            details[i][2] = files[i].substring(tail);
+            String[] sa = new String[3];
+            sa[0] = files[i].substring(0, head);
+            if (tail == files[i].length() - 1) {
+                sa[1] = files[i].substring(head);
+            } else {
+                sa[1] = files[i].substring(head, tail);
+                sa[2] = files[i].substring(tail);
+            }
+            details.put(i, sa);
+            entry[i] = i;
+        }
+        Arrays.sort(entry, (o1, o2) -> {
+            if (details.get(o1)[0].toLowerCase().equals(details.get(o2)[0].toLowerCase())) {
+                String s1 = details.get(o1)[1];
+                String s2 = details.get(o2)[1];
+                for(int i = 0; i < s1.length(); i++) {
+                    if (s1.charAt(i) > '0') {
+                        s1 = s1.substring(i);
+                        break;
+                    }
+                }
+                for(int i = 0; i < s2.length(); i++) {
+                    if (s2.charAt(i) > '0') {
+                        s2 = s2.substring(i);
+                        break;
+                    }
+                }
+                if (Integer.parseInt(s1) == (Integer.parseInt(s2))) {
+                    return 0;
+                } else {
+                    return Integer.parseInt(s1) - Integer.parseInt(s2);
+                }
+            } else {
+                return details.get(o1)[0].toLowerCase().compareTo(details.get(o2)[0].toLowerCase());
+            }
+        });
+        for(int i = 0; i < entry.length; i++) {
+            answer[i] = files[entry[i]];
         }
         return answer;
     }
